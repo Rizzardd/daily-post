405c5515392e3591a167391e1c6f66684a2aefc4
rizzarddirizu <87292423+Rizzardd@users.noreply.github.com>
2024-07-04 16:01:36 -0400

    refactor: adicionando estilizações ao componente de login.

--- daily-post-web/src/routes/auth/login.tsx
+++ daily-post-web/src/routes/auth/login.tsx
@@ -1,14 +1,30 @@
-import { Button, Container, Input } from "@chakra-ui/react";
 import { useMutation } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { http } from "../../infra/http";
 import { useState } from "react";
-import { resourceUsage } from "process";
 import { useAuthStore } from "../../stores/auth.store";
+import caralho from '../../assets/daily-post-stock.png';
 
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+  VStack,
+  HStack,
+  InputGroup,
+  InputLeftElement,
+  Image
+} from "@chakra-ui/react";
+
+import { EmailIcon, LockIcon } from "@chakra-ui/icons";
 export const Route = createFileRoute("/auth/login")({
   component: () => <LoginPage />,
 });
+
 
 function LoginPage() {
   const { accessToken, updateAccessToken } = useAuthStore();
@@ -33,26 +49,86 @@ function LoginPage() {
   });
 
   return (
-    <Container>
-    <div>
-      <Input value={email} onChange={(evt) => setEmail(evt.target.value)} />
-      <Input
-        type="password"
-        value={password}
-        onChange={(evt) => setPassword(evt.target.value)}
-      />
-      <Button
-        isLoading={isPending}
-        loadingText="Entrando..."
-        onClick={async () => {
-          const result = await mutateAsync({ email, password });
-          updateAccessToken(result);
-        }}
-      > 
-        Entrar 
-      </Button> 
-    </div>
-</Container>
-
+    <Center minH="100vh" bg="gray.800">
+      <Box
+        bg="gray.700"
+        p={8}
+        borderRadius="lg"
+        boxShadow="lg"
+        width="md"
+        color="white"
+      >
+        <VStack spacing={4} align="stretch">
+          <Text fontSize="2xl" fontWeight="bold">
+            Welcome to{" "}
+            <Text as="span" color="blue.500">
+              Daily Post!
+            </Text>
+          </Text>
+          <Text>Informe seu E-mail para Login</Text>
+          <FormControl id="email">
+            <FormLabel>E-mail</FormLabel>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <EmailIcon color="gray.300" />
+              </InputLeftElement>
+              <Input
+                type="email"
+                placeholder="dailypost@gmail.com"
+                value={email}
+                onChange={(evt) => setEmail(evt.target.value)}
+              />
+            </InputGroup>
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel>Senha</FormLabel>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <LockIcon color="gray.300" />
+              </InputLeftElement>
+              <Input
+                type="password"
+                placeholder="Senha"
+                value={password}
+                onChange={(evt) => setPassword(evt.target.value)}
+              />
+            </InputGroup>
+          </FormControl>
+          <Button
+            colorScheme="blue"
+            width="full"
+            isLoading={isPending}
+            loadingText="Entrando..."
+            onClick={async () => {
+              const result = await mutateAsync({ email, password });
+              updateAccessToken(result);
+            }}
+          >
+            Entrar
+          </Button>
+          <HStack justify="center" width="full">
+            <Text>ou</Text>
+          </HStack>
+          <Button colorScheme="blue" variant="outline" width="full">
+            Registrar
+          </Button>
+        </VStack>
+      </Box>
+      <Box
+        
+        display={{ base: "none", md: "flex" }}
+        alignItems="center"
+        justifyContent="center"
+        bg="backgroundLoginImg"
+        borderTopRightRadius="lg"
+        borderBottomRightRadius="lg"
+       width={262}
+   
+      >
+        <Box textAlign="center">
+        <Image src={caralho} width={80} mt={30} alt='FUNCIONA PUNHETA' />
+        </Box>
+      </Box>
+    </Center>
   );
 }
